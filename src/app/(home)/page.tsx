9e31b26fbbda2e3f12bc9cb7781f8053a03c63b8
kimzeze <fridaynight@kakao.com>
2024-08-04 22:30:15 +0900

    [🗼markup] 메인 페이지 타이틀 마크업

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,15 @@
-import KoreaMap from "../../components/KoreaMap";
+import KoreaMap from '../../components/KoreaMap';
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center align-middle">
+    <main className="flex justify-center">
+      <div className="flex items-center text-right h-[400px] border border-primary">
+        <h1 className="font-busan text-[80px] ">
+          <span className="block leading-[80px]">KOREA</span>
+          <span className="block leading-[50px]">RANDOM</span>
+          <span className="block leading-[80px]">TRAVEL</span>
+        </h1>
+      </div>
       <KoreaMap />
     </main>
   );
