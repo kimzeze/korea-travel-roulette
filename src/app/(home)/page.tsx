d97ea3bc65e34c8bd6d75a4bc95859b840d548e9
kimzeze <fridaynight@kakao.com>
2024-08-04 20:44:57 +0900

    [✨feat] 대한민국 광역시-도 별 룰렛 테스터 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,9 @@
+import KoreaMap from "../../components/KoreaMap";
+
 export default function Home() {
   return (
     <main className="flex justify-center items-center align-middle">
-      <h1>국내 여행지 룰렛 사이트</h1>
+      <KoreaMap />
     </main>
   );
 }
